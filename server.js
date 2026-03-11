ee6b3e1a41c8e8d902ad74b785c9c77645fbe884
ShiRa <shivaram0110@gmail.com>
2026-03-11 22:19:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/iotdb")
-.then(() => console.log("MongoDB connected"));
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
 app.use("/api", sensorRoutes);
 
