3a42c336167021d2b507a8af73bc15a9f74c833c
ShiRa <shivaram0110@gmail.com>
2026-03-11 22:30:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-
-const sensorRoutes = require("./routes/sensorRoutes");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
+// import routes
+const sensorRoutes = require("./routes/sensorRoutes");
+
+// use routes
+app.use("/api", sensorRoutes);
+
+// homepage message
+app.get("/", (req, res) => {
+  res.send("Sleep Apnea Monitoring Backend is Running");
+});
+
+// connect mongodb
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
-app.use("/api", sensorRoutes);
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
