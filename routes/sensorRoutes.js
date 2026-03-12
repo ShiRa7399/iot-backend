fbfb73c3f30b1940746c29667a0bef9570bb899d
ShiRa <shivaram0110@gmail.com>
2026-03-12 10:00:33 +0530

    Update sensorRoutes.js

--- routes/sensorRoutes.js
+++ routes/sensorRoutes.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+const Prediction = require("../models/Prediction");
+
+// get latest prediction
+router.get("/get-prediction", async (req, res) => {
+  try {
+
+    const latest = await Prediction
+      .findOne({ device_id: "esp32_01" })
+      .sort({ timestamp: -1 });
+
+    if (!latest) {
+      return res.json({ status: "No prediction yet" });
+    }
+
+    res.json({
+      status: latest.status
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
+
 const SensorData = require("../models/SensorData");
 
 router.post("/sensor-data", async (req, res) => {
