cf7559e1320dba1b7a5b0df05ca75f1059587cb1
ShiRa <shivaram0110@gmail.com>
2026-03-12 10:01:24 +0530

    Update Prediction.js

--- models/Prediction.js
+++ models/Prediction.js
@@ -1,15 +1,12 @@
-
 const mongoose = require("mongoose");
 
-const PredictionSchema = new mongoose.Schema({
-
-  risk: String,
-
-  created_at: {
-    type: Date,
-    default: Date.now
-  }
-
+const predictionSchema = new mongoose.Schema({
+  device_id: String,
+  status: String,
+  apnea_events: Number,
+  total_samples: Number,
+  apnea_ratio: Number,
+  timestamp: Date
 });
 
-module.exports = mongoose.model("Prediction", PredictionSchema);
+module.exports = mongoose.model("Prediction", predictionSchema);
